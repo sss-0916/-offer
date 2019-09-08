a0ad16a197d6905649d01c0a04d705f1def49709
sss-0916 <sss-0916@qq.com>
2019-09-08 13:25:40 +0800

    青蛙跳台阶

---
+++ 青蛙跳台阶.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int JumpFloor(int target) {
+        if(target == 1){
+            return 1;
+        }
+        
+        if(target == 2){
+            return 2;
+        }
+        
+        int result = 0;
+        int n1 = 2;
+        int n2 = 1;
+        for(int i = 3; i <= target; ++i){
+            result = n1 + n2;
+            n2 = n1;
+            n1 = result;
+        }
+        
+        return result;
+    }
+}
