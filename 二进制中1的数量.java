704a5a2a3a0caa0fa674ad19d8d1776c705728b9
sss-0916 <sss-0916@qq.com>
2019-09-12 08:05:42 +0800

    二进制中1的数量

---
+++ 二进制中1的数量.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int NumberOf1(int n) {
+        int cnt = 0;
+        
+        while(n != 0){
+            ++cnt;
+            n = n & (n - 1);
+        }
+        
+        return cnt;
+    }
+}
